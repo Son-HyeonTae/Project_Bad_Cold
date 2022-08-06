692a843e41cce814ae4c45d766faf317042e6b34
Son Hyeon Tae <104341400+Son-HyeonTae@users.noreply.github.com>
2022-08-06 22:48:45 +0900

    Delete Scripts directory

--- Scripts/GameScene/Player/Player.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Player : MonoBehaviour {
-    private int distance;
-    public  int Distance {
-        set => distance = Mathf.Max(0, value);
-        get => distance;
-    }
-}
--- Scripts/SceneChange.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class SceneChange : MonoBehaviour {
-    public void SceneLoader(string sceneName) {
-        SceneManager.LoadScene(sceneName);
-    }
-}
